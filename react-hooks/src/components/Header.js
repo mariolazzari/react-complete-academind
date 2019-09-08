424b30d2f3fd88c9c5a0544c80c3156457ef61fc
Mario Lazzari <mario.lazzari@gmail.com>
2019-09-08 17:23:47 +0200

    Done hooks lesson 13

---
+++ react-hooks/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <button>Todo list</button>
+      <button>Auth</button>
+    </header>
+  );
+};
+
+export default Header;
--- react-hooks/src/components/Todo.js
+++ react-hooks/src/components/Todo.js
@@ -1,24 +1,68 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 
-const Todo = props => {
-  const [todo, setTodo] = useState("");
+const Todo = () => {
+  // const [state, setState] = useState({ todo: "", todos: [] });
+  const [todo, setTodo] = useState({ title: "" });
   const [todos, setTodos] = useState([]);
 
-  const onChange = e => setTodo(e.target.value);
-  const addTodo = () => setTodos([...todos, todo]);
+  // fetch todos
+  useEffect(() => {
+    getTodos();
+
+    return () => {
+      console.log("clean up.");
+    };
+  }, []);
+
+  const mouseMoveHandler = e => console.log(e.clientX, e.clientY);
+
+  // cleanup
+  useEffect(() => {
+    document.addEventListener("mousemove", mouseMoveHandler);
+
+    return () => {
+      document.removeEventListener("mousemove", mouseMoveHandler);
+    };
+  }, []);
+
+  // load todos
+  const getTodos = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //const { todo, todos } = state;
+  const onChange = e => setTodo({ title: e.target.value });
+  //const onChange = e => setState({ ...state, todo: e.target.value });
+  const addTodo = async () => {
+    try {
+      setTodos([todo, ...todos]);
+      await axios.post("https://jsonplaceholder.typicode.com/todos", { todo });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //const addTodo = () => setState({ ...state, todos: [...todos, todo] });
 
   return (
     <Fragment>
       <input
         type="text"
         placeholder="Todo..."
-        value={todo}
+        value={todo.title}
         onChange={onChange}
       />
       <button onClick={addTodo}>Add</button>
       <ul>
         {todos.map((todo, i) => (
-          <li key={i}>{todo}</li>
+          <li key={i}>{todo.title}</li>
         ))}
       </ul>
     </Fragment>
